c73af97afd5b3b51b94859fe0a395977d739b682
Yevhenii Kurtov <me@lessess.pp.ua>
2013-12-06 16:48:40 +0200

    ember bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require skite
+
+// for more details see: http://emberjs.com/guides/application/
+window.Skite = Ember.Application.create()
+

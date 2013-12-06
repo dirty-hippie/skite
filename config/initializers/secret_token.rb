b9418b5aba4b6b2a3db876dc6c2a044fdb848327
Yevhenii Kurtov <me@lessess.pp.ua>
2013-12-06 15:49:07 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skite::Application.config.secret_key_base = '7bc0e176279b6415c4cff40e56446a3665c1b4f5526853ffad1cd621acf69c8cf0a0dd22b1c2b744253dbd492ce0872d5e918246913a111e4e74727ceaa0804a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skite::Application.config.session_store :cookie_store, key: '_skite_session'
